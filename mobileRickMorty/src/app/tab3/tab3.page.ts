ce8065e6777ddf9887a4c1c58c69662984b852af
EsterNeri <esterwwanderley@gmail.com>
2025-06-20 01:32:02 -0300

    Projeto Final

---
+++ mobileRickMorty/src/app/tab3/tab3.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+  standalone: false,
+})
+
+export class Tab3Page {
+  student = {
+    name: 'Ester Neri Vanderley',
+    email: 'esterwwanderley@gmail.com',
+    curso: 'Análise e Desenvolvimento de Sistemas',
+  };
+  constructor() {}
+}
